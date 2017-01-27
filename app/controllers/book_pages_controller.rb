4ec96830a34e180e0ca15dbb6ed6d3107d945fcc
Toni Češarek <programmertoni@gmail.com>
2017-01-27 15:30:33 +0100

    styles to styles output msg

--- app/controllers/book_pages_controller.rb
+++ app/controllers/book_pages_controller.rb
@@ -1,8 +1,6 @@
 class BookPagesController < ApplicationController
 
   def show
-    # http://localhost:3000/books/14/book_pages/30
-
     @book      = Book.find(params[:book_id])
     @book_page = @book.book_pages.order(:number).find(params[:id])
 
@@ -10,5 +8,4 @@ class BookPagesController < ApplicationController
       format.json { render json: { page_content: @book_page.content }} 
     end
   end
-
 end
