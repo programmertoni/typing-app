b5ecc6fc3f7cc1f75f85c875f825a58e874ac8a8
Toni Češarek <programmertoni@gmail.com>
2017-02-02 07:57:23 +0100

    additional styles to nav and success msg

--- app/controllers/os_projects_controller.rb
+++ app/controllers/os_projects_controller.rb
@@ -5,6 +5,5 @@ class OsProjectsController < ApplicationController
     @os_project    = OsProject.find(params[:id])
     @os_project_id = @os_project.id
     @page_ids      = @os_project.os_pages.map(&:id)
-
   end
 end
