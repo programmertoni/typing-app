48c8e002a653fd8d731eeffa6f30364f6ce055ec
Toni Češarek <programmertoni@gmail.com>
2017-02-03 16:15:00 +0100

    adds guard minitest-reposrters and login test

--- Gemfile
+++ Gemfile
@@ -36,8 +36,14 @@ end
 
 group :development do
   # Access an IRB console on exception pages or by using <%= console %> anywhere in the code.
+  gem 'guard'
+  gem 'guard-minitest'
   gem 'web-console', '>= 3.3.0'
   gem 'listen', '~> 3.0.5'
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
+end
+
+group :test do
+  gem 'minitest-reporters'
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  setup do
+    User.destroy_all
+    user          = User.new
+    user.name     = "admin"
+    user.password = "strong-password"
+    user.save
+  end
+
+  test "can login" do
+    admin_signs_in
+    get admin_books_path
+    assert_response :success
+  end
+
+  private
+
+  def admin_signs_in
+    post authenticate_user_path, params: { name: "admin", password: "strong-password" }
+  end
 end
