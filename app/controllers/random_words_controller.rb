705bbe55f42e1bc433b93bf1ccf5ced6c074ff51
Toni Češarek <programmertoni@gmail.com>
2017-02-09 06:23:00 +0100

    adds the rest of random generated feeds

--- app/controllers/random_words_controller.rb
+++ app/controllers/random_words_controller.rb
@@ -12,22 +12,21 @@ class RandomWordsController < ApplicationController
                      when "random: paragraph"    then LiterateRandomizer.paragraph
                      when "random: chuck norris" then Faker::ChuckNorris.fact
                      when "random: hipster"      then Faker::Hipster.paragraph
-                     when "random: hacker"       # https://github.com/stympy/faker/blob/master/doc/hacker.md
-                     when "random: ancients"     # https://github.com/stympy/faker/blob/master/doc/ancient.md
-                     when "random: Date"         # https://github.com/stympy/faker/blob/master/doc/date.md
-
+                     when "random: hacker"       then Faker::Hacker.say_something_smart
+                     when "random: ancients"     then from_list_to_sentance "Faker::Ancient.god", "Faker::Ancient.primordial", "Faker::Ancient.titan", "Faker::Ancient.hero"
+                     when "random: date"         then to_sentance "Faker::Date.between(100.years.ago, 100.years.from_now)", " "
+                     when "random: space"        then from_list_to_sentance "Faker::Space.planet", "Faker::Space.moon", "Faker::Space.galaxy", "Faker::Space.nebula", "Faker::Space.star_cluster", "Faker::Space.constellation", "Faker::Space.star", "Faker::Space.agency", "Faker::Space.nasa_space_craft", "Faker::Space.company", "Faker::Space.meteorite"
+                     when "random: food"         then from_list_to_sentance "Faker::Food.ingredient", "Faker::Food.spice"
                      when "random: names"        then to_sentance "Faker::Name.name"
                      when "random: addresses"    then to_sentance "Faker::Address.street_address"
                      when "random: cities"       then to_sentance "Faker::Address.city"
-                     when "random: countries"      then to_sentance "Faker::Address.country"
-                     when "random: space"        # https://github.com/stympy/faker/blob/master/doc/space.md
+                     when "random: countries"    then to_sentance "Faker::Address.country"
                      when "random: superhero"    then to_sentance "Faker::Superhero.name"
-                     when "random: email"        # Faker::Internet.email                                      # => "eliza@mann.net"
-                     when "random: numbers"      # Faker::Number.number(10)                                   # => "1968353479"
-                     when "random: websites"     # Faker::Internet.domain_name                                # => "effertz.info"
-                     when "random: url"          # Faker::Internet.url                                        # => "http://thiel.com/chauncey_simonis"
-                     when "random: characters"   # Faker::Internet.password(10, 20, true, true)               # => "*%NkOnJsH4"
-                     when "random: food"         # https://github.com/stympy/faker/blob/master/doc/food.md
+                     when "random: email"        then to_sentance "Faker::Internet.email"
+                     when "random: numbers"      then to_sentance "Faker::Number.number(10)", " "
+                     when "random: websites"     then to_sentance "Faker::Internet.domain_name", " "
+                     when "random: url"          then to_sentance "Faker::Internet.url"
+                     when "random: characters"   then to_sentance "Faker::Internet.password(10, 20, true, true)", " "
                      when "radnom: [{()}]"
                      end
 
@@ -38,10 +37,15 @@ class RandomWordsController < ApplicationController
 
   private
 
-  def to_sentance(phrase)
-    arr = []
-    10.times { arr << eval(phrase) }
-    arr.join(", ")
+  def to_sentance(phrase, separator = ', ')
+    result = []
+    12.times { result << eval(phrase) }
+    result.join(separator)
   end
 
+  def from_list_to_sentance(*list)
+    result = []
+    list.cycle(5) { |x| result << eval(x) }
+    result.join(', ')
+  end
 end
